2c8bfb9da0034aeea3b56000767630c1087c9901
daitxsuelen <suelendaitx@yahoo.com.br>
2024-08-14 21:27:00 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int x, y;
+double divisao;
+
+Console.WriteLine("Digite o valor de X:");
+x = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Digite o valor de y");
+y = Convert.ToInt32(Console.ReadLine());
+
+divisao = (double) x / y;
+
+Console.WriteLine($"O valor da divisão entre {x} e {y} é igual a: {divisao}");
